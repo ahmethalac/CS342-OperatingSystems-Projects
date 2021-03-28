2951deee98f25ee70b322d9d31cdb827866d2d49
ahmethalac <ahmetfeyzihalac@gmail.com>
2021-03-28 20:51:21 +0300

    Project 2 :: Implementation

--- Project 2/schedule.c
+++ Project 2/schedule.c
@@ -5,106 +5,302 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/queue.h>
+#include <sys/time.h>
+#include <limits.h>
 
-//GLobal variables
-int count = 0;
-
-//Struct definitions
-struct args {
+// Struct definitions
+struct wThreadArgs {
     int threadID;
-    int readFromFile; //boolean (0 for random burst, 1 for read from file)
-    char* inprefix;
-    int Bcount;
+    int readFromFile; // boolean (0 for random burst, 1 for read from file)
+    char* inprefix; // Filename prefix
+    int Bcount; // Burst count
     int minB, avgB, minA, avgA;
+} typedef wThreadArgs;
+
+struct sThreadArgs {
+    int N;
     char* ALG;
-} typedef threadArgs;
+} typedef sThreadArgs;
 
 struct burst {
     int threadIndex;
     int burstIndex;
     int length;
-    int generationTime;
-} typedef burst;
+    long generationTime;
+    SLIST_ENTRY(burst) next;
+} typedef burstNode;
 
-//Helper functions
-double expRandom(double mean) {
-    return -mean * log(rand() / (RAND_MAX + 1.0));
+// Global variables
+int count = 0;
+int done = 0;
+long timeOffset; // Initial time of main function
+struct rqHead head; // head of runqueue
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t rqEmpty = PTHREAD_COND_INITIALIZER;
+
+SLIST_HEAD(rqHead, burst);
+
+// Helper functions
+double expRandom(unsigned int* seed, double mean) {
+    return -mean * log(rand_r(seed) / (RAND_MAX + 1.0)); // Using inverse method
 }
 
-//Thread functions
-void* W_thread(void* args) {
-    int ID = ((threadArgs*)args)->threadID;
-    int readFromFile = ((threadArgs*)args)->readFromFile;
-    char* inprefix = ((threadArgs*)args)->inprefix;
-    int Bcount = ((threadArgs*)args)->Bcount;
-    int minB = ((threadArgs*)args)->minB;
-    int avgB = ((threadArgs*)args)->avgB;
-    int minA = ((threadArgs*)args)->minA;
-    int avgA = ((threadArgs*)args)->avgA;
-    char* ALG = ((threadArgs*)args)->ALG;
+long getCurrentTime() {
+    struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    return (currentTime.tv_sec * 1000000 + currentTime.tv_usec) / 1000;
+}
 
-    if (readFromFile == 1) {
-        FILE *fp;
-        char* line = NULL;
-        size_t len = 0;
-        ssize_t read;
+void createBurst(int threadIndex, int burstIndex, int length, int sleepAmount) {
+    printf("[%ld] %dth thread sleeps for %d\n",
+           getCurrentTime() - timeOffset, threadIndex, sleepAmount);
 
-        char fileName[strlen(inprefix) + 7];
-        char format[strlen(inprefix)];
-        strcpy(format, inprefix);
-        sprintf(fileName, strcat(format, "-%d.txt"), ID);
+    usleep(sleepAmount * 1000);
 
-        fp = fopen(fileName, "r");
-        while ((read = getline(&line, &len, fp)) != -1) {
-            int sleepAmount, burstAmount;
-            sscanf(line, "%d %d", &sleepAmount, &burstAmount);
+    burstNode* temp = malloc(sizeof(burstNode));
+    temp->threadIndex = threadIndex;
+    temp->burstIndex = burstIndex;
+    temp->length = length;
 
-            printf("ThreadID: %d, Sleep Amount: %d\n", ID, sleepAmount);
-            usleep(sleepAmount * 1000);
+    pthread_mutex_lock(&lock);
 
-            printf("ThreadID: %d, Burst Amount: %d\n", ID, burstAmount);
+    temp->generationTime = getCurrentTime();
+    printf("[%ld] %d.%d(%d) is created\n",
+           getCurrentTime() - timeOffset, threadIndex, burstIndex, length);
+
+    // Add burst to the end of runqueue
+    if (!SLIST_EMPTY(&head)) {
+        burstNode* last = SLIST_FIRST(&head);
+        while (SLIST_NEXT(last, next)) {
+            last = SLIST_NEXT(last, next);
         }
-        free(line);
+        SLIST_INSERT_AFTER(last, temp, next);
+    } else {
+        SLIST_INSERT_HEAD(&head, temp, next);
+    }
 
-    } else if (readFromFile == 0){
-        srand(ID * time(NULL));
-        for (int i = 1; i < Bcount + 1; i++) {
-            int sleepAmount;
-            do {
-                sleepAmount = (int)expRandom((double)avgA);
-            } while (sleepAmount < minA);
-            printf("ThreadID: %d, Sleep Amount: %d\n", ID, sleepAmount);
-            usleep(sleepAmount * 1000);
+    pthread_cond_signal(&rqEmpty); // Signal S_thread about burst creation
+    pthread_mutex_unlock(&lock);
+}
 
-            int burstAmount;
-            do {
-                burstAmount = (int)expRandom((double)avgB);
-            } while (burstAmount < minB);
-            printf("ThreadID: %d, Burst Amount: %d\n", ID, burstAmount);
+void createBurstsFromFile(char* inprefix, int ID) {
+    FILE *fp;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    char fileName[strlen(inprefix) + 7];
+    char format[strlen(inprefix)];
+    strcpy(format, inprefix);
+    sprintf(fileName, strcat(format, "-%d.txt"), ID);
+
+    fp = fopen(fileName, "r");
+    int burstIndex = 1;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        int sleepAmount, burstAmount;
+        sscanf(line, "%d %d", &sleepAmount, &burstAmount);
+
+        createBurst(ID, burstIndex, burstAmount, sleepAmount);
+        burstIndex++;
+    }
+    free(line);
+}
+
+burstNode* sjfAlgorithm(int N) {
+    burstNode* chosenBurst;
+
+    //For each thread, keep the information that whether earliest burst of this thread is found (1 if found)
+    int foundEarliestBurst[N];
+    for (int i = 0; i < N; ++i) {
+        foundEarliestBurst[i] = 0;
+    }
+
+    burstNode* foundBurst;
+    int minBurst = INT_MAX;
+    SLIST_FOREACH(foundBurst, &head, next) {
+        if (foundEarliestBurst[foundBurst->threadIndex - 1] == 1) {
+            continue;
+        }
+        foundEarliestBurst[foundBurst->threadIndex - 1] = 1;
+        if (foundBurst->length < minBurst) {
+            chosenBurst = foundBurst;
+            minBurst = foundBurst->length;
         }
     }
 
-    pthread_exit(0);
+    return chosenBurst;
+}
+
+burstNode* prioAlgorithm(int N) {
+    burstNode* chosenBurst;
+
+    //For each thread, keep the information that whether earliest burst of this thread is found (1 if found)
+    int foundEarliestBurst[N];
+    for (int i = 0; i < N; ++i) {
+        foundEarliestBurst[i] = 0;
+    }
+
+    burstNode* foundBurst;
+    int highestPriority = N + 1;
+    SLIST_FOREACH(foundBurst, &head, next) {
+        if (foundEarliestBurst[foundBurst->threadIndex - 1] == 1) {
+            continue;
+        }
+        foundEarliestBurst[foundBurst->threadIndex - 1] = 1;
+        if (foundBurst->threadIndex < highestPriority) {
+            chosenBurst = foundBurst;
+            highestPriority = foundBurst->threadIndex;
+        }
+    }
+
+    return chosenBurst;
+}
+
+burstNode* vruntimeAlgorithm(int* vruntime, int N) {
+    burstNode* chosenBurst;
+
+    //For each thread, keep the information that whether earliest burst of this thread is found (1 if found)
+    int foundEarliestBurst[N];
+    for (int i = 0; i < N; ++i) {
+        foundEarliestBurst[i] = 0;
+    }
+
+    burstNode* foundBurst;
+    int minVruntime = INT_MAX;
+    SLIST_FOREACH(foundBurst, &head, next) {
+        if (foundEarliestBurst[foundBurst->threadIndex - 1] == 1) {
+            continue;
+        }
+        foundEarliestBurst[foundBurst->threadIndex - 1] = 1;
+        if (vruntime[foundBurst->threadIndex - 1] < minVruntime) {
+            chosenBurst = foundBurst;
+            minVruntime = vruntime[foundBurst->threadIndex - 1];
+        }
+    }
+    vruntime[chosenBurst->threadIndex - 1] += chosenBurst->length * (0.7 + 0.3 * chosenBurst->threadIndex);
+    return chosenBurst;
+}
+
+// Thread runners
+void* W_thread(void* args) {
+    // Get args
+    int ID = ((wThreadArgs*)args)->threadID;
+    int readFromFile = ((wThreadArgs*)args)->readFromFile;
+    char* inprefix = ((wThreadArgs*)args)->inprefix;
+    int Bcount = ((wThreadArgs*)args)->Bcount;
+    int minB = ((wThreadArgs*)args)->minB;
+    int avgB = ((wThreadArgs*)args)->avgB;
+    int minA = ((wThreadArgs*)args)->minA;
+    int avgA = ((wThreadArgs*)args)->avgA;
+
+    // Set different seed for each thread
+    unsigned int seed = ID;
+
+    if (readFromFile == 1) { //Read bursts from file
+        createBurstsFromFile(inprefix, ID);
+    } else if (readFromFile == 0){
+        for (int i = 1; i < Bcount + 1; i++) {
+            int sleepAmount;
+            do {
+                sleepAmount = (int)expRandom(&seed, (double)avgA);
+            } while (sleepAmount < minA);
+
+            int burstAmount;
+            do {
+                burstAmount = (int)expRandom(&seed, (double)avgB);
+            } while (burstAmount < minB);
+
+            createBurst(ID, i, burstAmount, sleepAmount);
+        }
+    }
+    return 0;
+}
+
+void* S_thread(void* args) {
+    // Get args
+    int N = ((sThreadArgs*)args)->N;
+    char* ALG = ((sThreadArgs*)args)->ALG;
+
+    int vruntime[N], burstCount[N], totalWaitingTime[N];
+    for (int i = 0; i < N; ++i) {
+        vruntime[i] = 0;
+        burstCount[i] = 0;
+        totalWaitingTime[i] = 0;
+    }
+
+    while (done == 0 || !SLIST_EMPTY(&head)) {
+        pthread_mutex_lock(&lock);
+        if (SLIST_EMPTY(&head)) {
+            printf("[%ld] Waiting for a burst in S_thread\n", getCurrentTime() - timeOffset);
+            pthread_cond_wait(&rqEmpty, &lock);
+            printf("[%ld] New burst arrived, finish waiting in S_thread\n", getCurrentTime() - timeOffset);
+        }
+
+        burstNode* chosenBurst;
+        if (strcmp(ALG, "FCFS") == 0) {
+            chosenBurst = SLIST_FIRST(&head);
+        } else if (strcmp(ALG, "SJF") == 0) {
+            chosenBurst = sjfAlgorithm(N);
+        } else if (strcmp(ALG, "PRIO") == 0) {
+            chosenBurst = prioAlgorithm(N);
+        } else if (strcmp(ALG, "VRUNTIME") == 0) {
+            chosenBurst = vruntimeAlgorithm(vruntime, N);
+        }
+
+        burstCount[chosenBurst->threadIndex - 1]++;
+        totalWaitingTime[chosenBurst->threadIndex - 1] += getCurrentTime() - chosenBurst->generationTime;
+        SLIST_REMOVE(&head, chosenBurst, burst, next);
+
+        printf("[%ld] Running %d.%d(%d) -- ",
+               getCurrentTime() - timeOffset, chosenBurst->threadIndex, chosenBurst->burstIndex, chosenBurst->length);
+        printf("Waiting time is: %ld\n", getCurrentTime() - chosenBurst->generationTime);
+
+        pthread_mutex_unlock(&lock);
+        usleep(chosenBurst->length * 1000);
+        free(chosenBurst);
+    }
+
+    printf("\nAverage waiting times:\n");
+    int totalSum = 0;
+    int totalBurstCount = 0;
+    for (int i = 0; i < N; ++i) {
+        printf("Thread %d: %.2f\n", i + 1, totalWaitingTime[i] / (double) burstCount[i]);
+        totalBurstCount += burstCount[i];
+        totalSum += totalWaitingTime[i];
+    }
+    printf("Total average waiting time: %.2f\n", totalSum / (double)totalBurstCount );
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
+    // Initial preparations
+    timeOffset = getCurrentTime();
+    SLIST_INIT(&head);
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("Failure of mutex init\n");
+        return 0;
+    }
     int N = atoi(argv[1]);
-    char* ALG;
+    sThreadArgs* sThreadArg = (sThreadArgs*)malloc(sizeof(sThreadArgs));
+    sThreadArg->N = N;
+
     pthread_t tid[N];
+    pthread_t s_thread_id;
+    wThreadArgs* args[N];
+    char* ALG;
+
     if (argc == 5) { //Get bursts from file
         ALG = argv[2];
-        char* inprefix = argv[4];
+        sThreadArg->ALG = ALG;
+        pthread_create(&s_thread_id, NULL, S_thread, sThreadArg); // S_Thread
 
-        for (int i = 1; i < N + 1; i++) {
-            threadArgs* args = (threadArgs*)malloc(sizeof(threadArgs));
-            args->readFromFile = 1;
-            args->inprefix = inprefix;
-            args->ALG = ALG;
-            args->threadID = i;
-            pthread_create(&tid[i - 1], NULL, W_thread, (void*)args);
-        }
-        for (int i = 1; i < N + 1; ++i) {
-            pthread_join(tid[i - 1], NULL);
+        for (int i = 0; i < N; i++) { // W_Threads
+            args[i] = (wThreadArgs*)malloc(sizeof(wThreadArgs));
+            args[i]->readFromFile = 1;
+            args[i]->inprefix = argv[4];
+            args[i]->threadID = i + 1;
+            pthread_create(&tid[i], NULL, W_thread, (void*)args[i]);
         }
     } else {
         int Bcount = atoi(argv[2]), minB = atoi(argv[3]),
@@ -112,21 +308,35 @@ int main(int argc, char *argv[]) {
             avgA = atoi(argv[6]);
         ALG = argv[7];
 
-        for (int i = 1; i < N + 1; i++) {
-            threadArgs* args = (threadArgs*)malloc(sizeof(threadArgs));
-            args->readFromFile = 0;
-            args->Bcount = Bcount;
-            args->minB = minB;
-            args->avgB = avgB;
-            args->minA = minA;
-            args->avgA = avgA;
-            args->ALG = ALG;
-            args->threadID = i;
-            pthread_create(&tid[i - 1], NULL, W_thread, (void*)args);
-        }
-        for (int i = 1; i < N + 1; ++i) {
-            pthread_join(tid[i - 1], NULL);
+        sThreadArg->ALG = ALG;
+        pthread_create(&s_thread_id, NULL, S_thread, sThreadArg); // S_Thread
+
+        for (int i = 0; i < N; i++) { // W_Threads
+            args[i] = (wThreadArgs*)malloc(sizeof(wThreadArgs));
+            args[i]->readFromFile = 0;
+            args[i]->Bcount = Bcount;
+            args[i]->minB = minB;
+            args[i]->avgB = avgB;
+            args[i]->minA = minA;
+            args[i]->avgA = avgA;
+            args[i]->threadID = i + 1;
+            pthread_create(&tid[i], NULL, W_thread, (void*)args[i]);
         }
     }
+    // Join W_Threads and free memory allocations
+    for (int i = 0; i < N; ++i) {
+        pthread_join(tid[i], NULL);
+    }
+    for (int i = 0; i < N; ++i) {
+        free(args[i]);
+    }
+    done = 1;
+
+    // Join S_Thread and free memory allocation
+    pthread_join(s_thread_id, NULL);
+    free(sThreadArg);
+
+    pthread_mutex_destroy(&lock);
+
     return 0;
 }
